2ebcdf0854dbe6fc4384a4526ae7733cb7c6875f
JON <jonn.thinkrtc@gmail.com>
2018-01-25 14:26:00 -0800

    Initial commit

---
+++ app/src/main/java/com/udacity/jonathan/practicejavaset2/MainActivity.java
@@ -0,0 +1,28 @@
+package com.udacity.jonathan.practicejavaset2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int dollars = 40;
+        int dollarsToYen = 119;
+        int yen = dollarsToYen * dollars;
+        display(yen);
+
+
+    }
+    public void display (int i) {
+        TextView t = (TextView) findViewById(R.id.display_text_view);
+        t.setText(""+i);
+    }
+
+
+}
