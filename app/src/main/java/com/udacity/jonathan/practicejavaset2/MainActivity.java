c97280dea2554b786a64bf35c599b626ff66166d
JON <jonn.thinkrtc@gmail.com>
2018-01-25 14:37:23 -0800

    Email directory algorithm problem using Java

--- app/src/main/java/com/udacity/jonathan/practicejavaset2/MainActivity.java
+++ app/src/main/java/com/udacity/jonathan/practicejavaset2/MainActivity.java
@@ -12,14 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int dollars = 40;
-        int dollarsToYen = 119;
-        int yen = dollarsToYen * dollars;
-        display(yen);
+       String firstName = "Lyla";
+       String lastName = "Fujiwara";
+       String contactInfo = firstName + " " + lastName;
+       contactInfo = contactInfo + "<" + lastName + "." + firstName + "@justjava.com>";
+       display(contactInfo);
 
 
     }
-    public void display (int i) {
+    public void display (String i) {
         TextView t = (TextView) findViewById(R.id.display_text_view);
         t.setText(""+i);
     }
